2edb15f583c75ab158d167863d64f8d19d13ca61
Ezekiel-faith <ezekielfaith2000@gmail.com>
2025-08-25 05:03:05 +0100

    finishing touches

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,13 +1,23 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 import { GlobalStyles } from '../../constants/styles';
 
-export default function ExpensesOutput({ expenses, expensesPeriod }) {
+export default function ExpensesOutput({
+  expenses,
+  expensesPeriod,
+  fallBackText,
+}) {
+  let content = <Text style={styles.infoText}>{fallBackText}</Text>;
+
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
+
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList expenses={expenses} />
+      {content}
     </View>
   );
 }
@@ -19,5 +29,12 @@ const styles = {
     paddingTop: 24,
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32,
   },
 };
