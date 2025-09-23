3d4a843410545c0ac2885eceeb4eb933aef39ee1
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-09-23 06:52:18 +0100

    setting & using default values

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -11,6 +11,10 @@ export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? 'Edit Expense' : 'Add Expense',
@@ -41,6 +45,7 @@ export default function ManageExpense({ route, navigation }) {
         submitButtonLabel={isEditing ? 'Update' : 'Add'}
         onSubmit={confirmHandler}
         onCancel={cancelHandler}
+        defaultValues={selectedExpense}
       />
 
       {isEditing && (
